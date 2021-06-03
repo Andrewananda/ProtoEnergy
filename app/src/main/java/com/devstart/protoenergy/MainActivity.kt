d9300ce6dac2a00d005b7f02c28cd942f336acf8
andrew.tiema <andrewtiema@gmail.com>
2021-06-03 12:48:06 +0300

    feature/migrated to navigation component

--- app/src/main/java/com/devstart/protoenergy/MainActivity.kt
+++ app/src/main/java/com/devstart/protoenergy/MainActivity.kt
@@ -2,51 +2,13 @@ package com.devstart.protoenergy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.lifecycleScope
-import com.devstart.protoenergy.network.ApiResponse
-import com.devstart.protoenergy.network.Failure
-import com.devstart.protoenergy.network.Success
-import com.devstart.protoenergy.orders.viewModel.OrderViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var viewModel: OrderViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fetchData()
-    }
-
-    private fun fetchData() {
-        lifecycleScope.launch {
-            viewModel.fetchOrders().collect {
-                when(it) {
-                    is Failure -> {
-                        logFailiure(it.throwable)
-                    }
-                    is Success<*> -> {
-                        logSuccess(it)
-                    }
-                }
-            }
-        }
-    }
-
-    private fun logSuccess(response: ApiResponse) {
-        Log.i("Success", response.toString())
-    }
-
-    private fun logFailiure(failure: Throwable) {
-        Log.i("Failure", failure.localizedMessage)
     }
 }
---
+++ app/src/main/java/com/devstart/protoenergy/orders/ui/OrdersFragment.kt
@@ -0,0 +1,55 @@
+package com.devstart.protoenergy.orders.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.devstart.protoenergy.R
+import com.devstart.protoenergy.network.ApiResponse
+import com.devstart.protoenergy.network.Failure
+import com.devstart.protoenergy.network.Success
+import com.devstart.protoenergy.orders.viewModel.OrderViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class OrdersFragment : Fragment() {
+    @Inject
+    lateinit var viewModel: OrderViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        fetchData()
+        return inflater.inflate(R.layout.fragment_orders, container, false)
+    }
+
+    private fun fetchData() {
+        lifecycleScope.launchWhenCreated {
+            viewModel.fetchOrders().collect { res ->
+                when(res) {
+                    is Failure -> {
+                        logFailiure(res.throwable)
+                    }
+                    is Success<*> -> {
+                        logSuccess(res)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun logSuccess(response: ApiResponse) {
+        Log.i("Success", response.toString())
+    }
+
+    private fun logFailiure(failure: Throwable) {
+        Log.i("Failure", failure.localizedMessage)
+    }
+}
