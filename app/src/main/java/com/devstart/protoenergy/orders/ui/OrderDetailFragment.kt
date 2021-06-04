151f735302e73a2417549bfc1965f0d4d38171ae
andrew.tiema <andrewtiema@gmail.com>
2021-06-04 11:39:13 +0300

    fix/xml

--- app/src/main/java/com/devstart/protoenergy/orders/ui/OrderDetailFragment.kt
+++ app/src/main/java/com/devstart/protoenergy/orders/ui/OrderDetailFragment.kt
@@ -1,24 +1,26 @@
 package com.devstart.protoenergy.orders.ui
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.devstart.protoenergy.R
+import com.devstart.protoenergy.databinding.FragmentOrderDetailBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class OrderDetailFragment : BottomSheetDialogFragment( ) {
 
+    private lateinit var binding: FragmentOrderDetailBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_order_detail, container, false)
         val bundle = arguments
         val orderDetails = bundle?.let { OrderDetailFragmentArgs.fromBundle(it) }
-        Log.i("Bundle", orderDetails.toString())
+        binding.order = orderDetails?.orderDetails
 
-        return inflater.inflate(R.layout.fragment_order_detail, container, false)
+        return binding.root
     }
 }
