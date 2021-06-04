014acfff904b30d42fb7463a29b1080833ed87fd
andrew.tiema <andrewtiema@gmail.com>
2021-06-05 01:15:26 +0300

    feature/dialer

--- app/src/main/java/com/devstart/protoenergy/orders/ui/OrderDetailFragment.kt
+++ app/src/main/java/com/devstart/protoenergy/orders/ui/OrderDetailFragment.kt
@@ -1,5 +1,7 @@
 package com.devstart.protoenergy.orders.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +9,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.devstart.protoenergy.R
 import com.devstart.protoenergy.databinding.FragmentOrderDetailBinding
+import com.devstart.protoenergy.orders.model.Order
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class OrderDetailFragment : BottomSheetDialogFragment( ) {
@@ -17,10 +20,22 @@ class OrderDetailFragment : BottomSheetDialogFragment( ) {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_order_detail, container, false)
-        val bundle = arguments
-        val orderDetails = bundle?.let { OrderDetailFragmentArgs.fromBundle(it) }
-        binding.order = orderDetails?.orderDetails
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bundle = arguments
+        val orderObject = bundle?.let { OrderDetailFragmentArgs.fromBundle(it) }
+        binding.order = orderObject?.orderDetails
+
+        binding.floatingActionButton.setOnClickListener {
+            val value: Order? = orderObject?.orderDetails
+
+            value?.userPhoneNumber0.let {
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + value?.userPhoneNumber0))
+                startActivity(intent)
+            }
+        }
     }
 }
