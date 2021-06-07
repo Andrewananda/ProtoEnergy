2416956fb469520cdaeed279599bfcd39140eed3
andrew.tiema <andrewtiema@gmail.com>
2021-06-07 14:51:21 +0300

    fix/binding

--- app/src/main/java/com/devstart/protoenergy/orders/ui/OrderDetailFragment.kt
+++ app/src/main/java/com/devstart/protoenergy/orders/ui/OrderDetailFragment.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import com.devstart.protoenergy.R
 import com.devstart.protoenergy.databinding.FragmentOrderDetailBinding
 import com.devstart.protoenergy.orders.model.Order
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
